ec02b7f4d4f9c4a994ac1f99b223a3e2b7bb13bd
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-08-08 15:23:41 +0200

    Updated kernel core

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,24 @@
 #include "kernel.h"
 #include "stdio.h"
 
-/* extern */ int kMain(void* bootData) {
+/* extern */ int kMain(void* bootData)
+{
     // do some stuff here
     clear_();
-    puts_(makeColor(GREEN, BLACK), "Kernel started!");
+    
+    unsigned char const whiteOnBlack = makeColor(WHITE, BLACK),
+                        greenOnBlack = makeColor(GREEN, BLACK),
+                        redOnBlack   = makeColor(RED,   BLACK);
+                        
+    puts_(whiteOnBlack, "[      ] Booting kernel...\n");
+    
+    // load everything there
+    // for now let's just wait a bit
+    for (unsigned long i = 0; i < 1000000000L; ++i);
+    
+    puts_(whiteOnBlack, "[");
+    puts_(greenOnBlack, "  OK  ");
+    puts_(whiteOnBlack, "] Booted kernel!\n");
     
     while (1); // never ending loop, cause we don't want the kernel to just stop
     
--- src/kernel.h
+++ src/kernel.h
@@ -3,6 +3,10 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+/**
+ * Kernel entry point
+ * Everything will launch from there
+ */
 extern int kMain(void* bootData);
 
 #endif
