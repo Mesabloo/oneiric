2095c012640f134cf189427e6a66581e0357e838
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-08-19 00:28:11 +0200

    Added multiboot support (partial)

---
+++ c/include/core/multiboot.h
@@ -0,0 +1,153 @@
+#pragma once
+#ifndef MULTIBOOT_H
+#define MULTIBOOT_H
+
+#include <std/stddef.h>
+
+/* The symbol table for a.out. */
+struct multiboot_aout_symbol_table
+{
+    uint32_t tabsize;
+    uint32_t strsize;
+    uint32_t addr;
+    uint32_t reserved;
+};
+typedef struct multiboot_aout_symbol_table multiboot_aout_symbol_table_t;
+
+/* The section header table for ELF. */
+struct multiboot_elf_section_header_table
+{
+    uint32_t num;
+    uint32_t size;
+    uint32_t addr;
+    uint32_t shndx;
+};
+typedef struct multiboot_elf_section_header_table multiboot_elf_section_header_table_t;
+
+struct multiboot_info
+{
+    /* Multiboot info version number */
+    uint32_t flags;
+
+    /* Available memory from BIOS */
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+
+    /* "root" partition */
+    uint32_t boot_device;
+
+    /* Kernel command line */
+    uint32_t cmdline;
+
+    /* Boot-Module list */
+    uint32_t mods_count;
+    uint32_t mods_addr;
+
+    union {
+        multiboot_aout_symbol_table_t aout_sym;
+        multiboot_elf_section_header_table_t elf_sec;
+    } u;
+
+    /* Memory Mapping buffer */
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+
+    /* Drive Info buffer */
+    uint32_t drives_length;
+    uint32_t drives_addr;
+
+    /* ROM configuration table */
+    uint32_t config_table;
+
+    /* Boot Loader Name */
+    uint32_t boot_loader_name;
+
+    /* APM table */
+    uint32_t apm_table;
+
+    /* Video */
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t framebuffer_bpp;
+#define MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED 0
+#define MULTIBOOT_FRAMEBUFFER_TYPE_RGB 1
+#define MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT 2
+    uint8_t framebuffer_type;
+    union {
+        struct
+        {
+            uint32_t framebuffer_palette_addr;
+            uint16_t framebuffer_palette_num_colors;
+        };
+        struct
+        {
+            uint8_t framebuffer_red_field_position;
+            uint8_t framebuffer_red_mask_size;
+            uint8_t framebuffer_green_field_position;
+            uint8_t framebuffer_green_mask_size;
+            uint8_t framebuffer_blue_field_position;
+            uint8_t framebuffer_blue_mask_size;
+        };
+    };
+};
+typedef struct multiboot_info multiboot_info_t;
+
+struct multiboot_color
+{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+};
+
+struct multiboot_mmap_entry
+{
+    uint32_t size;
+    uint64_t addr;
+    uint64_t len;
+#define MULTIBOOT_MEMORY_AVAILABLE 1
+#define MULTIBOOT_MEMORY_RESERVED 2
+#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE 3
+#define MULTIBOOT_MEMORY_NVS 4
+#define MULTIBOOT_MEMORY_BADRAM 5
+    uint32_t type;
+} __attribute__((packed));
+typedef struct multiboot_mmap_entry multiboot_memory_map_t;
+
+struct multiboot_mod_list
+{
+    /* the memory used goes from bytes ’mod_start’ to ’mod_end-1’ inclusive */
+    uint32_t mod_start;
+    uint32_t mod_end;
+
+    /* Module command line */
+    uint32_t cmdline;
+
+    /* padding to take it to 16 bytes (must be zero) */
+    uint32_t pad;
+};
+typedef struct multiboot_mod_list multiboot_module_t;
+
+/* APM BIOS info. */
+struct multiboot_apm_info
+{
+    uint16_t version;
+    uint16_t cseg;
+    uint32_t offset;
+    uint16_t cseg_16;
+    uint16_t dseg;
+    uint16_t flags;
+    uint16_t cseg_len;
+    uint16_t cseg_16_len;
+    uint16_t dseg_len;
+};
+
+#endif
--- c/src/core/kernel.c
+++ c/src/core/kernel.c
@@ -2,21 +2,42 @@
 #include <std/io/console.h>
 #include <std/stddef.h>
 #include <std/stderr.h>
+#include <core/multiboot.h>
+#include <std/stdstr.h>
 
-/* extern */ void kMain()
+static logLine = 0;
+
+/* extern */ void kMain(multiboot_info_t* mb_info)
 {
+    assert(mb_info);
+    if (mb_info->flags & (1 << 12) && mb_info->framebuffer_type == MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT)
+        initDisplay(mb_info->framebuffer_width, mb_info->framebuffer_height);
+
     clear();
-    moveCursorAt(80 - 19, 24);
+
+    if(mb_info->flags & (1 << 12) && mb_info->framebuffer_type == MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT)
+    {
+        moveCursorAt(0, logLine++);
+        char buf[5], buf1[5];
+        memset(buf, 0, 5); memset(buf1, 0, 5);
+
+        putsN("\e\x8C[\e\x01 INFO \e\x8C] Initialized display with size "
+             , uint_to_str(mb_info->framebuffer_width, buf, 10)
+             , "x", uint_to_str(mb_info->framebuffer_height, buf1, 10)
+             , 0);
+    }
+
+    moveCursorAt(getDisplaySizeX() - 19, getDisplaySizeY() - 1);
     puts("\e\x8C Booting kernel... ");
 
     asm inline(
         "movl $500000000000, %ecx\n"
         ".L5: loop .L5\n"
     );
-    moveCursorAt(0, 0);
+    moveCursorAt(0, logLine++);
     puts("\e\x07[\e\x0A  OK  \e\x07] Nothing to activate right now... (will come soon)\n");
 
-    moveCursorAt(80 - 19, 24);
+    moveCursorAt(getDisplaySizeX() - 19, getDisplaySizeY() - 1);
     puts("\e\x8A Booted kernel!    ");
 
     asm inline(
@@ -26,5 +47,7 @@
 
     //terminate("This kernel is not fully implemented yet!", 0x0);
 
-    while (1);
+    asm inline(
+        "wait: jmp wait\n"
+    );
 }
